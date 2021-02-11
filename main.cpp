3e96c244cfc11294e9ae014e5d5d868d906c2ee4
Svetlana Zilpausha <szilpausha@gmail.com>
2021-02-11 17:17:28 +0000

    Taisnstūra paralēlskaldņa tilpums

--- main.cpp
+++ main.cpp
@@ -1,13 +1,34 @@
 #include <iostream>
 using namespace std;
-// programmai ir jānolasa skaitlis N un jāizvada šis skaitlis pareizināts ar 2
+// Taisnstūra paralēlskaldņa tilpums
 int main() {
-  int x;
-  int reiz;
-  cout << "Ievadi skaitli : ";
-  cin >> x;
-  
-  reiz = x * 2;
-  cout << "Skaitļu reizināšana ar 2 ir: " << reiz;
-  return 0;
-}
+  int a, b, c;
+  int V;
+  cout << "Ievadi a: ";
+  cin >> a;
+    if (a < 0)
+  {
+    cout << "Skaiļim jābut pozitīvam!";
+  }
+    else {
+    cout << "Ievadi b: ";
+    cin >> b;
+    if (b < 0)
+  {
+    cout << "Skaiļim jābut pozitīvam!";
+  }
+    else {
+    cout << "Ievadi c: ";
+    cin >> c;
+        }
+  if (c < 0)
+      {
+    cout << "Skaiļim jābut pozitīvam!";
+       }
+  else {
+    V = a * b * c;
+    cout << "Taisnstūra paralēlskaldņa tilpums ir: " << V;
+    return 0;
+        }
+  }
+        }
